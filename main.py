bf1e47ade4cb4a41dc3c12916c2823fccb0bb87d
Anna Nowak <annadnowak98@gmail.com>
2021-11-24 20:28:22 +0100

    Speakato trainer skeleton (moved)

---
+++ main.py
@@ -0,0 +1,27 @@
+import datetime
+from trainer import SpeakatoTrainer
+
+model_path = f"../models/Speakato_model_{datetime.datetime.now().timestamp()}"
+dataset_path = f"../dataset/training_data.json"
+language = "pl"
+
+print("Welcome to speakato trainer! Before we start, let's configure some neccessary stuff!")
+new_path = input(f"Where do you want to save your model? Default path: {model_path}")
+if new_path:
+    model_path = new_path
+
+new_path = input(f"Where is dataset? Default path: {dataset_path}")
+if new_path:
+    dataset_path = new_path
+
+new_language = input(f"What language you want to use (pl/eng)? Default language: {language}")
+if(new_language not in ["pl", "eng"]):
+    language = new_language
+
+print(f"""
+    Config summary:
+    Model path: {model_path}
+    Dataset path: {dataset_path}
+""")
+
+speakato_trainer = SpeakatoTrainer(language, model_path, dataset_path)
